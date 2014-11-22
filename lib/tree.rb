694510702f39a79144538bacd6106d55fbd4e106
Benjamin Madueme <bmadueme@gmail.com>
2014-11-22 14:03:57 -0500

    Implemented tree.rb, and added tests to tree_spec.rb

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,38 +1,86 @@
 class NoApplesError < StandardError; end
 
-class AppleTree
-  attr_#fill_in :height, :age, :apples, :alive
+class Tree
+  attr_accessor :height, :age, :apples, :alive
 
   def initialize
+    @height = 0
+    @age = 0
+    @apples = []
+    @alive = true
   end
-
+  
   def age!
+    if @age == 100
+      @alive = false
+      @apples = []
+    else
+
+      #Grow really rapidly when it's young, but grows at a slower rate
+      #as it gets older.  It stops growing altogether at a certain point.
+      #The tree starts growing fruit at a certain point, but continues
+      #growing fruit until it dies.
+      case @age
+
+      #Note: .. for ranges is inclusive for both values; ... for ranges means we exclude the end value
+      when (0...5)
+        @height = @height + 2.5
+      when (5...10)
+        @height = @height + 2
+        rand(75..100).times { add_apple }
+      when (10...15)
+        @height = @height + 1.5
+        rand(150..250).times { add_apple }
+      when (15...25)
+        @height = @height + 1
+        rand(250..500).times { add_apple }
+      when (25...50)
+        @height = @height + 0.5
+        rand(150..250).times { add_apple }
+      else
+        rand(75..100).times { add_apple }
+      end
+
+      #Finally, age the tree
+      @age = @age + 1
+    end
   end
 
-  def add_apples
+  def add_apple
+    #Never allow the apple tree to have more than 1000 apples
+    if @apples.length < 1000
+      @apples << Apple.new("Red", rand(5..10))
+    end
   end
 
   def any_apples?
+    return true if @apples.length > 0
+    false
   end
 
   def pick_an_apple!
-    raise NoApplesError, "This tree has no oranges" unless self.any_apples?
+    raise NoApplesError, "This tree has no apples" unless self.any_apples?
+    return @apples.pop
   end
 
   def dead?
+    @alive ? false : true
   end
 end
 
 class Fruit
+  attr_accessor :has_seeds
   def initialize
-    has_seeds = true
+    @has_seeds = true
   end
 end
 
-class Apple <
-  attr_reader #what should go here 
+class Apple < Fruit
+  attr_accessor :color, :diameter
 
   def initialize(color, diameter)
+    @color = color
+    @diameter = diameter
   end
 end
 
@@ -43,7 +91,7 @@ end
 def tree_data
   tree = Tree.new
 
-  tree.age! until tree.any_apple?
+  tree.age! until tree.any_apples?
 
   puts "Tree is #{tree.age} years old and #{tree.height} feet tall"
 
@@ -61,11 +109,11 @@ def tree_data
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    avg_diameter = (diameter_sum.to_f / basket.length).round(2)
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
-    puts "Harvest:     #{basket.size} oranges with an average diameter of #{avg_diameter} inches"
+    puts "Harvest:     #{basket.size} apples with an average diameter of #{avg_diameter} inches"
     puts ""
 
     # Ages the tree another year
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,14 +1,75 @@
 require 'rspec'
 require 'tree'
+require 'pry'
 
-describe 'Tree' do
+describe Tree do
+  let(:tree) { Tree.new }
+
   it 'should be a Class' do
-    expect(described_class.is_a? 'Class').to be_true
+    expect(described_class.is_a? Class).to be true
+  end
+
+  it 'should have an age method' do
+    expect(described_class.method_defined? :age).to be true
+  end
+
+  it 'should age by 1 year if age is called' do
+    tree.age!
+    expect(tree.age).to eq 1
+  end
+
+  it 'should have one apple if we add an apple' do
+    tree.add_apple
+    expect(tree.any_apples?).to be true
+  end
+
+  it 'should have no apples if we pick an apple' do
+    tree.add_apple
+    tree.pick_an_apple!
+    expect(tree.apples.length).to eq 0
+  end
+
+  it 'should bear more than 50 apples at age 6' do
+    6.times { tree.age! }
+    expect(tree.apples.length).to be > 50 
+  end
+
+  it 'should never have more than 1000 apples, no matter how many times add_apple is called' do
+    59809.times { tree.add_apple }
+    expect(tree.apples.length).to eq 1000
+  end
+
+  it 'should be dead after age 100' do
+    101.times{ tree.age! }
+    expect(tree.dead?).to be true
+  end
+
+end
+
+describe Fruit do
+  let(:fruit) { Fruit.new }
+
+	it 'should be a Class' do
+    expect(described_class.is_a? Class).to be true
+  end
+
+  it 'should have seeds' do
+    expect(fruit.has_seeds).to be true
   end
 end
 
-describe 'Fruit' do
-end
+describe Apple do
+  let(:apple) { Apple.new("Red", 7) }
 
-describe 'Apple' do
+  it 'should be a Class' do
+    expect(described_class.is_a? Class).to be true
+  end
+
+  it 'should be color Red' do
+    expect(apple.color).to eq "Red"
+  end
+
+  it 'should have a diameter of 7 inches' do
+    expect(apple.diameter).to eq 7
+  end
 end
