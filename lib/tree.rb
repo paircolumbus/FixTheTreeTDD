41c6eff615b4f9bd1d6acbf0012cfbb3f3e70abd
Nicolas McCurdy <thenickperson@gmail.com>
2015-01-09 17:07:28 -0500

    Nick's solution

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,38 +1,69 @@
 class NoApplesError < StandardError; end
 
-class AppleTree
-  attr_#fill_in :height, :age, :apples, :alive
+class Tree
+  attr_reader :height, :age, :alive
 
   def initialize
+    @height = 0
+    @age = 0
+    @alive = true
+  end
+end
+
+class AppleTree < Tree
+  attr_reader :apples
+
+  def initialize
+    super
+    @apples = []
   end
 
   def age!
+    case @age
+    when 0...5
+      @age += 1
+      @height += 1
+    when 5...10
+      @age += 1
+      add_apples
+    else
+      @alive = false
+    end
   end
 
   def add_apples
+    @apples << Apple.new('green', 3)
   end
 
   def any_apples?
+    @apples.size > 0
   end
 
   def pick_an_apple!
-    raise NoApplesError, "This tree has no oranges" unless self.any_apples?
+    raise NoApplesError, "This tree has no apples" unless self.any_apples?
+    @apples.pop
   end
 
   def dead?
+    !@alive
   end
 end
 
 class Fruit
+  attr_reader :has_seeds
+
   def initialize
-    has_seeds = true
+    @has_seeds = true
   end
 end
 
-class Apple <
-  attr_reader #what should go here 
+class Apple < Fruit
+  attr_reader :color, :diameter
 
   def initialize(color, diameter)
+    super()
+    @color = color
+    @diameter = diameter
   end
 end
 
@@ -41,9 +72,9 @@ end
 # it should calculate the diameter of the apples in the basket
 
 def tree_data
-  tree = Tree.new
+  tree = AppleTree.new
 
-  tree.age! until tree.any_apple?
+  tree.age! until tree.any_apples?
 
   puts "Tree is #{tree.age} years old and #{tree.height} feet tall"
 
@@ -58,14 +89,16 @@ def tree_data
     diameter_sum = 0
 
     basket.each do |apple|
+      diameter_sum
+      apple.diameter
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    avg_diameter = diameter_sum / basket.size # It's up to you to calculate the average diameter for this harvest.
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
-    puts "Harvest:     #{basket.size} oranges with an average diameter of #{avg_diameter} inches"
+    puts "Harvest:     #{basket.size} apples with an average diameter of #{avg_diameter} inches"
     puts ""
 
     # Ages the tree another year
