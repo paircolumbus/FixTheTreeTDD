2453a47ffe640db55aecb0e92d59d469cd466072
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-22 10:17:00 -0400

    Empty specs (pretty much)

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -2,13 +2,27 @@ require 'rspec'
 require 'tree'
 
 describe 'Tree' do
-  it 'should be a Class' do
-    expect(described_class.is_a? 'Class').to be_true
+  before(:each) do
+    @tree = AppleTree.new
   end
+
+  it 'has tests'
+
+  # it 'should be a Class' do
+  #   expect(described_class.is_a? 'Class').to be_true
+  # end
+
+  # it 'should age' do
+  #   before_age = @tree.age
+  #   @tree.age!
+  #   expect(@tree.age).to eq(before_age + 1)
+  # end
 end
 
 describe 'Fruit' do
+  it 'has tests'
 end
 
 describe 'Apple' do
+  it 'has tests'
 end
