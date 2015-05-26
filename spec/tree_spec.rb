2f5c406c4bcb5dc2b69bce48086bc93f2865b3e7
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-26 14:48:07 -0400

    Switched before to let

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -2,49 +2,47 @@ require 'rspec'
 require 'tree'
 
 describe 'Tree' do
-  before(:each) do
-    @tree = AppleTree.new
-  end
+  let(:tree) { AppleTree.new }
 
   it 'should age' do
-    before_age = @tree.age
-    @tree.age!
-    expect(@tree.age).to eq(before_age + 1)
+    before_age = tree.age
+    tree.age!
+    expect(tree.age).to eq(before_age + 1)
   end
 
   it 'should add apples' do
-    before_count = @tree.apples.count
-    @tree.add_apples
-    expect(@tree.apples.count).to be > before_count
+    before_count = tree.apples.count
+    tree.add_apples
+    expect(tree.apples.count).to be > before_count
   end
 
   it 'should originally have no apples' do
-    expect(@tree.any_apples?).to eq(false)
+    expect(tree.any_apples?).to eq(false)
   end
 
   it 'should be able to have apples' do
-    @tree.apples << Apple.new('red', 5)
-    expect(@tree.any_apples?).to be true
+    tree.apples << Apple.new('red', 5)
+    expect(tree.any_apples?).to be true
   end
 
   it 'should not let you pick no apples' do
-    expect{@tree.pick_an_apple!}.to raise_error(NoApplesError)
+    expect{tree.pick_an_apple!}.to raise_error(NoApplesError)
   end
 
   it 'should let you to pick an apple' do
-    @tree.add_apples
-    apple = @tree.pick_an_apple!
+    tree.add_apples
+    apple = tree.pick_an_apple!
 
     expect(apple.is_a? Apple).to be true
   end
 
   it 'should originally be alive' do
-    expect(@tree.dead?).to eq(false)
+    expect(tree.dead?).to eq(false)
   end
 
   it 'should die' do
-    @tree.alive = false
-    expect(@tree.dead?).to be true
+    tree.alive = false
+    expect(tree.dead?).to be true
   end
 end
 
