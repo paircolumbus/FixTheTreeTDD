350968eab15cd2ddc068cc5c8e4d6fc4d5b7b0b1
Lovinder Pnag <lpnag@CMDVMOSXLT075.local>
2015-05-26 10:20:51 -0400

    finished tests and working apple tree with printouts

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,14 +1,49 @@
 require 'rspec'
 require 'tree'
 
-describe 'Tree' do
-  it 'should be a Class' do
-    expect(described_class.is_a? 'Class').to be_true
-  end
-end
+describe 'AppleTree' do
+	let(:tree) { AppleTree.new }	
 
+	describe '#age' do 
+		it 'should show the age of the tree' do 
+			expect(tree.age).to eq(0)
+		end
+	end
+
+	describe '#height' do 
+		it 'should show the height of the tree' do 
+			expect(tree.height).to eq(0)
+		end
+	end
+
+	describe '#age!' do
+		it 'should change the age' do
+			expect{tree.age!}.to change{tree.age}.from(0).to(1)
+		end
+
+		it 'should make the tree grow' do 
+			expect{tree.age!}.to change{tree.height}.by(3)
+		end
+
+		it 'should cause the tree to eventually die' do 
+			99.times {tree.age!}
+			expect(tree.alive?).to eq(false)
+		end
+	end
+end
+	
 describe 'Fruit' do
 end
 
 describe 'Apple' do
+	let(:apple) {Apple.new("red", 3)}
+
+	it 'should show the color of the apple' do
+		expect(apple.color).to eq("red")
+	end
+
+	it 'should show the diameter of the apple' do
+		expect(apple.diameter).to eq(3)
+	end
+
 end
