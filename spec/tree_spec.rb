a7fb7640f5b9000b412529d92b9b9cc7618e994d
Vasanth <vasanth.pappu@gmail.com>
2015-07-04 17:36:31 -0400

    failing test for initial age

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -15,6 +15,15 @@ describe AppleTree do
   it 'should be a subclass of Tree' do
     expect(described_class < Tree).to eq true
   end
+  context 'when initialized' do
+    subject do
+      AppleTree.new
+    end
+    it 'should start at 0 years of age' do
+      expect(subject.age).to eq 0
+    end
+
+  end
 end
 
 describe 'Fruit' do
