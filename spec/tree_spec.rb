0c9e2c55edd8fdf227980e6441bd0b4c31b5e7a3
Vasanth <vasanth.pappu@gmail.com>
2015-07-04 17:48:59 -0400

    failing test for #age!

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -12,18 +12,36 @@ describe AppleTree do
   it 'should be a Class' do
     expect(described_class.is_a? Class).to eq true
   end
+
   it 'should be a subclass of Tree' do
     expect(described_class < Tree).to eq true
   end
+
+  subject do
+    AppleTree.new
+  end
+
   context 'when initialized' do
-    subject do
-      AppleTree.new
-    end
+
     it 'should start at 0 years of age' do
       expect(subject.age).to eq 0
     end
 
+    it 'it can start at any year of age specified' do
+      object = AppleTree.new(age: 100)
+      expect(object.age).to eq 100
+    end
+
   end
+
+
+  describe '#age!' do
+    it 'increases the age by 1' do
+      subject.age!
+      expect(subject.age).to eq 1
+    end
+  end
+
 end
 
 describe 'Fruit' do
