4eaf0937bb0390b07b0325d1f0d5b3fa9ac5f775
Gabriel Escamilla <GEscamilla@covermymeds.com>
2015-09-29 13:51:19 -0400

    Added init tests

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -9,6 +9,27 @@ describe Tree do
     expect(described_class.is_a? Class).to be true
   end
 
+  context "when initialized" do
+
+    it "gives the default height" do
+      expect(tree.height).to eq(1)
+    end
+
+    it "gives the default age" do
+      expect(tree.age).to eq(0)
+    end
+
+    it "has no apples at the begining" do
+      expect(tree.any_apples?).to be false
+    end
+
+    it "is alive by default" do
+      expect(tree.dead?).to be false
+    end
+
+  end
+
+
 end
 
 describe Fruit do
