554d7bbcded6824a07339ddb1191f2d4bbc32c2d
nate anderson <nathanaelcanderson@gmail.com>
2016-03-04 13:52:40 -0500

    about the change logic re @apples variable

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,12 +2,16 @@ class NoApplesError < StandardError; end
 
 module AppleTree
   
+  # adds apples when @apples == true, creates an array of 100 - 250 apples, diameter between 3 and 5 
   def add_apples
+    harvest = Array.new(rand(100..250)) { Apple.new }
   end
 
+  # boolean value, checks if class basket is empty 
   def any_apples?
   end
 
+  # removes an apple from apple array which will be added to basket array, if [] is empty?, any_apples? == false, 
   def pick_an_apple!
     raise NoApplesError, "This tree has no apples" unless self.any_apples?
   end
@@ -15,33 +19,62 @@ module AppleTree
 end
 
 class Tree
+
   include AppleTree
 
-  attr_accessor#fill_in :height, :age, :apples, :alive
-
+  attr_accessor :height, :age, :apples, :alive
+  
   def initialize
+    @height = 0
+    @age = 0
+    @apples = false
+    @alive = true
   end
 
   def age!
+    @age += 1
+    if @age >= 5
+      @apples = true
+    end
+
+    @height += 1 unless @height == 30
+
+    if @age >= 150
+      @alive = false
+    end
   end
 
+  # returns true when @alive == false
   def dead?
+    if @alive == false
+      true
+    else
+      false
+    end
   end
 
 
 end
 
 class Fruit
+
+  # creates new fruit object, used by apple below. grows new tree if seeds.viable?
   def initialize
     has_seeds = true
   end
+
 end
 
+# inherits from fruit
 class Apple 
-  #attr_reader #what should go here 
+  attr_reader :color, :diameter 
 
-  def initialize(color, diameter)
+  # creates new apple of a color and diameter, diameter based on age of tree
+  def initialize#(color, diameter)
+    @color = color
+    @diameter = diameter
   end
+
 end
 
 #THERES ONLY ONE THING YOU NEED TO EDIT BELOW THIS LINE
@@ -69,7 +102,7 @@ def tree_data
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    # avg_diameter = # It's up to you to calculate the average diameter for this harvest.
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
@@ -85,3 +118,8 @@ end
 
 # Uncomment this line to run the script, but BE SURE to comment it before you try to run your tests!
 # tree_data
+
+
+tree = Tree.new
+10.times { tree.age! }
+p tree
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,34 +1,110 @@
 require 'rspec'
 require 'tree'
 
-describe 'Tree' do
+describe "Tree" do
 
   before :each do
     @tree = Tree.new
   end
 
-  it 'should be a Class' do
+
+  it "should be a Class" do
     expect(Tree).to be_a Class
   end
 
+  it "class includes the AppleTree module" do
+    expect { @tree.add_apples }.to_not raise_error
+  end
+
   describe "#new" do
+
     it "returns a new Tree object" do
       expect(@tree).to be_an_instance_of(Tree)
     end
+
+    it "is instantiated with 4 instance variables" do
+      expect(@tree.instance_variable_defined?(:@height)).to be true
+      expect(@tree.instance_variable_defined?(:@age)).to be true
+      expect(@tree.instance_variable_defined?(:@apples)).to be true
+      expect(@tree.instance_variable_defined?(:@alive)).to be true
+    end
+
+  end
+
+
+  describe "#age!" do
+
+    it "ages the tree object one year per call" do
+      10.times { @tree.age! }
+      expect(@tree.age).to eq(10)
+    end
+
+    it "adds apples to the tree when the tree is old enough" do
+      if @tree.age >= 5
+        expect(@tree.apples).to be true
+      end
+    end
+
+    it "grows the tree 1 foot per year" do
+      10.times { @tree.age! }
+      expect(@tree.height).to eq(10)
+    end
+
+    it "stops growing the tree when reaching a height of 30 feet" do
+      40.times { @tree.age! }
+      expect(@tree.height).to eq(30)
+    end
+
+    it "sets @alive to false if the tree reaches 100 years old" do
+      if @tree.age >= 150
+      expect(@tree.alive).to be false
+      end
+    end
+
+  end
+
+  describe "#dead?" do
+
+    it "evaluates to true when @alive evaluates to false" do
+      150.times { @tree.age! }
+      expect(@tree.dead?).to be true
+    end
+
   end
 
 end
 
-describe 'AppleTree' do
+describe "AppleTree" do
 
-  it 'should be a module' do
+  before :each do
+    @tree = Tree.new
+    @tree.add_apples
+  end
+
+  it "should be a module" do
     expect(AppleTree).to be_a Module
   end
 
+  describe "#add_apples" do
+    it "creates an array of 100 -250 apple objects, called 'harvest', when @apples == true" do
+      @tree.add_apples
+      expect(@tree.apples).to be_a Array
+    end
+
+    it "gives the apples in the array a diamter between 3 and 5" do
+    end
+
+    it "gives the apples in the array a color" do
+    end
+
+  end
+
+
+
 end
 
 
-describe 'Fruit' do
+describe "Fruit" do
 
   it 'should be a Class' do
     expect(Fruit).to be_a Class
