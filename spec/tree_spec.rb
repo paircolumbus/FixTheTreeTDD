2d252205349d608e0e9f53da3ee84f527975f3d4
Josh Graber <jgraber@covermymeds.com>
2016-06-11 15:07:59 -0400

    Add tree tests
    Add basic tests for trees.

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,6 +1,28 @@
 require 'rspec'
 require 'tree'
 
+describe Tree do
+  context "Tree with history" do
+    let(:tree_heights) { [1, 12, 37, 49, 65] }
+    let(:history) { TreeHistory.new(tree_heights) }
+    let(:tree) { Tree.new(history) }
+
+    it "should live 5 years" do
+      expect(tree.dead?).to eq false
+
+      5.times { tree.age! }
+      expect(tree.dead?).to eq true
+    end
+
+    it "should change height each year" do
+      tree_heights.each do |height|
+        expect(tree.height).to eq height
+        tree.age!
+      end
+    end
+  end
+end
+
 describe AppleTree do
   let(:apple_tree) { AppleTree.new }
 
