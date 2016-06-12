dac077aaf904414e751d8794c950c022e15cd248
Josh Meek <jmeek@covermymeds.com>
2016-06-12 17:55:59 -0400

    add initial tests/tree rb file

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,25 +1,41 @@
 class NoApplesError < StandardError; end
 
-class AppleTree
-  attr_#fill_in :height, :age, :apples, :alive
-
+class Tree
+  attr_accessor :height, :age, :alive
+  TREE_DEATH_AGE = 60
   def initialize
+    @height = 0
+    @age = 0
+    @alive = true
   end
 
   def age!
-  end
-
-  def add_apples
-  end
-
-  def any_apples?
-  end
-
-  def pick_an_apple!
-    raise NoApplesError, "This tree has no apples" unless self.any_apples?
+    @age += 1
   end
 
   def dead?
+    @age > TREE_DEATH_AGE
+  end
+end
+
+class AppleTree < Tree
+  attr_reader :apples
+
+  def initialize
+    super
+    @apples = []
+  end
+
+  def add_apples(apples)
+    @apples.concat apples
+  end
+
+  def any_apples?
+    apples.any?
+  end
+
+  def pick_an_apple
+    raise NoApplesError, 'This tree has no apples' unless any_apples?
   end
 end
 
@@ -29,19 +45,22 @@ class Fruit
   end
 end
 
-class Apple <
-  attr_reader #what should go here 
+class Apple < Fruit
+  attr_reader :color, :diameter
 
   def initialize(color, diameter)
+    super()
+    @color = color
+    @diamter = diameter
   end
 end
 
-#THERES ONLY ONE THING YOU NEED TO EDIT BELOW THIS LINE
+# THERES ONLY ONE THING YOU NEED TO EDIT BELOW THIS LINE
 # avg_diameter (line 58) will raise an error.
 # it should calculate the diameter of the apples in the basket
 
 def tree_data
-  tree = Tree.new
+  tree = AppleTree.new
 
   tree.age! until tree.any_apples?
 
@@ -51,9 +70,7 @@ def tree_data
     basket = []
 
     # It places the apple in the basket
-    while tree.any_apples?
-      basket << tree.pick_an_apple!
-    end
+    basket << tree.pick_an_apple! while tree.any_apples?
 
     diameter_sum = 0
 
@@ -61,18 +78,18 @@ def tree_data
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    avg_diameter = 0 # It's up to you to calculate the average diameter for this harvest.
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
     puts "Harvest:     #{basket.size} apples with an average diameter of #{avg_diameter} inches"
-    puts ""
+    puts ''
 
     # Ages the tree another year
     tree.age!
   end
 
-  puts "Alas, the tree, she is dead!"
+  puts 'Alas, the tree, she is dead!'
 end
 
 # Uncomment this line to run the script, but BE SURE to comment it before you try to run your tests!
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,14 +1,86 @@
 require 'rspec'
 require 'tree'
 
-describe 'Tree' do
+describe Tree do
   it 'should be a Class' do
-    expect(described_class.is_a? 'Class').to be_true
+    expect(described_class.is_a?(Class)).to be true
+  end
+
+  it 'should initialize' do
+    tree = Tree.new
+    expect(tree.age).to be 0
+    expect(tree.height).to be 0
+    expect(tree.alive).to be true
+  end
+
+  it 'should age' do
+    tree = Tree.new
+    expect(tree.age).to be 0
+    tree.age!
+    expect(tree.age).to be 1
+  end
+
+  it 'should not be dead' do
+    tree = Tree.new
+    expect(tree.age).to be 0
+    expect(tree.dead?).to be false
+    tree.age!
+    expect(tree.age).to be 1
+    expect(tree.dead?).to be false
+  end
+
+  it 'should be dead' do
+    tree = Tree.new
+    expected_age = 0
+    expect(tree.age).to be 0
+    expect(tree.dead?).to be false
+    60.times do
+      tree.age!
+      expected_age += 1
+
+      expect(tree.age).to be expected_age
+      expect(tree.dead?).to be false
+    end
+    tree.age!
+    expect(tree.age).to be 61
+    expect(tree.dead?).to be true
   end
 end
 
-describe 'Fruit' do
+describe AppleTree do
+  it 'should be a Class' do
+    expect(described_class.is_a?(Class)).to be true
+  end
+
+  it 'should initialize' do
+    tree = AppleTree.new
+    expect(tree.age).to be 0
+    expect(tree.height).to be 0
+    expect(tree.alive).to be true
+    expect(tree.apples.count).to be 0
+  end
+
+  it 'should not have apples' do
+    tree = AppleTree.new
+    expect(tree.any_apples?).to be false
+  end
+
+  it 'should add an apple' do
+    tree = AppleTree.new
+    expect(tree.any_apples?).to be false
+    tree.add_apples Array(Apple.new('Blue,', 10))
+    expect(tree.any_apples?).to be true
+  end
 end
 
-describe 'Apple' do
+describe Fruit do
+  it 'should be a class' do
+    expect(described_class.is_a?(Class)).to be true
+  end
+end
+
+describe Apple do
+  it 'should be a class' do
+    expect(described_class.is_a?(Class)).to be true
+  end
 end
