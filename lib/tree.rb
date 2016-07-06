2c6b481817592cef2acf294b6a9e2d4f252bb8bf
Liz Cymanski <eacymanski@gmail.com>
2016-07-06 09:41:38 -0400

    completed

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,38 +1,55 @@
 class NoApplesError < StandardError; end
 
-class AppleTree
-  attr_#fill_in :height, :age, :apples, :alive
+class Tree
+  attr_accessor :height, :age, :apples, :alive
 
   def initialize
+    @height=0
+    @age=0
+    @apples=[]
+    @alive=true
   end
 
   def age!
+    @age+=1
+    @alive = false if age > rand(50..100)
+    @age<15 ? @height+=3 : @height+=1
+    apple_num = rand(5..15)
+    apple_num.times {add_apples} if @age >10 
   end
 
   def add_apples
+    colors =["red","yellow","green"]
+    @apples << Apple.new(colors.sample,rand(3..13))
   end
 
   def any_apples?
+    @apples.count > 0
   end
 
   def pick_an_apple!
     raise NoApplesError, "This tree has no apples" unless self.any_apples?
+    @apples.pop
   end
 
   def dead?
+    !@alive
   end
 end
 
 class Fruit
+  attr_accessor :has_seeds
   def initialize
-    has_seeds = true
+    @has_seeds = true
   end
 end
 
-class Apple <
-  attr_reader #what should go here 
+class Apple < Fruit
+  attr_reader :diameter, :color
 
   def initialize(color, diameter)
+    @color=color
+    @diameter=diameter
   end
 end
 
@@ -46,6 +63,8 @@ def tree_data
   tree.age! until tree.any_apples?
 
   puts "Tree is #{tree.age} years old and #{tree.height} feet tall"
+
+  puts "Tree has #{tree.apples.count} apples"
 
   until tree.dead?
     basket = []
@@ -61,7 +80,8 @@ def tree_data
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    
+   avg_diameter = diameter_sum/basket.count
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
@@ -76,4 +96,4 @@ def tree_data
 end
 
 # Uncomment this line to run the script, but BE SURE to comment it before you try to run your tests!
-# tree_data
+#tree_data
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -2,13 +2,106 @@ require 'rspec'
 require 'tree'
 
 describe 'Tree' do
+  let(:tree) {Tree.new}
   it 'should be a Class' do
-    expect(described_class.is_a? 'Class').to be_true
+    expect(tree.is_a? Tree).to be true
   end
+
+  it 'should have an age of 0' do
+    expect(tree.age).to eq 0 
+  end
+  
+  it 'should have a height of 0' do
+    expect(tree.height).to eq 0 
+  end
+
+  it 'should have no apples' do
+    expect(tree.apples.count).to eq 0 
+  end
+
+  it 'should be alive' do 
+    expect(tree.alive).to eq true
+  end
+
+  context 'aging' do
+    it 'should increment age' do
+      tree.age!
+      expect(tree.age).to eq 1
+    end
+
+    it 'should increment height' do
+      tree.age!
+      expect(tree.height).to eq 3
+    end
+
+    it 'should no grow apples' do
+      tree.age!
+      expect(tree.apples.count).to eq 0 
+    end
+
+    it 'should die before 100' do
+      100.times {tree.age!}
+      expect(tree.dead?).to be true 
+    end
+
+    it 'should not die before 50' do
+      49.times {tree.age!}
+      expect(tree.alive).to be true
+    end
+
+    it 'should grow 1 unit after 15' do
+      15.times{tree.age!}
+      height = tree.height
+      tree.age!
+      expect(tree.height-height).to eq 1
+    end
+
+    it 'should grow apples after 10 years' do
+      11.times{tree.age!}
+      expect(tree.any_apples?).to be true
+    end
+  end
+
+  context 'picking apples'
+    it 'should remove one apple' do
+      11.times{tree.age!}
+      num_apples = tree.apples.count
+      tree.pick_an_apple!
+      expect(num_apples-tree.apples.count).to eq  1
+    end
 end
 
 describe 'Fruit' do
+  let(:fruit) {Fruit.new}
+
+  it 'should be a fruit' do
+    expect(fruit.is_a? Fruit).to be true
+  end
+
+  it 'should have seeds' do
+    expect(fruit.has_seeds).to be true
+  end
+
 end
 
 describe 'Apple' do
+  let(:apple) {Apple.new('red', 10)}
+
+  it 'should be an apple' do
+    expect(apple.is_a? Apple).to be true
+  end
+
+  it 'should inherit from fruit' do
+    expect(apple.is_a? Fruit).to be true
+  end
+
+  it 'should be red' do
+    expect(apple.color).to eq "red"
+  end
+
+  it 'should have a diameter of 10' do
+    expect(apple.diameter).to eq 10
+  end
+
+
 end
