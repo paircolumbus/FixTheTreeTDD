27cbc68da26051b6a2ae8b1e257eb80f627745e9
drm2 <davidrmyersii@gmail.com>
2016-11-22 13:38:20 -0500

    Making sure quotes are uniform.

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -10,14 +10,14 @@ describe Tree do
         expect(described_class).to eq(Tree)
     end
 
-    describe "#initialize" do
-        it "will generate a living tree" do
+    describe '#initialize' do
+        it 'will generate a living tree' do
             expect(tree.alive).to eq(true) # ask about be_true
         end
     end
 
-    describe "#age!" do
-        it "will age the tree by 1 year" do
+    describe '#age!' do
+        it 'will age the tree by 1 year' do
             age = tree.age
 
             tree.age!
@@ -25,7 +25,7 @@ describe Tree do
             expect(tree.age).to eq(age + 1)
         end
 
-        it "will increase the height by 5 to 10 feet" do
+        it 'will increase the height by 5 to 10 feet' do
             height = tree.height
 
             tree.age!
@@ -33,7 +33,7 @@ describe Tree do
             expect(tree.height).to be_between(height + 5, height + 10)
         end
 
-        it "will add 1 to 6 apples" do
+        it 'will add 1 to 6 apples' do
             apples_count = tree.apples.count
 
             tree.age!
@@ -41,7 +41,7 @@ describe Tree do
             expect(tree.apples.count).to be_between(apples_count + 1, apples_count + 6)
         end
 
-        it "will eventually die" do
+        it 'will eventually die' do
             tree.age_max.times do
                 tree.age!
             end
@@ -50,8 +50,8 @@ describe Tree do
         end
     end
 
-    describe "#add_apples!" do
-        it "adds between 1 and 6 apples to the tree" do
+    describe '#add_apples!' do
+        it 'adds between 1 and 6 apples to the tree' do
             apples_count = tree.apples.count
 
             tree.add_apples!
@@ -61,23 +61,23 @@ describe Tree do
     end
 
     describe '#any_apples?' do
-        it "returns true if the tree has apples" do
+        it 'returns true if the tree has apples' do
             tree.add_apples
 
             expect(tree.any_apples?).to eq(true)
         end
 
-        it "returns false if tree has no apples" do
+        it 'returns false if tree has no apples' do
             expect(tree.any_apples?).to eq(false)
         end
     end
 
-    describe "#pick_an_apple!" do
-        it "throws a NoApplesError when there are no apples" do
+    describe '#pick_an_apple!' do
+        it 'throws a NoApplesError when there are no apples' do
             expect{ tree.pick_an_apple! }.to raise_error(NoApplesError) # this block didn't work without braces, need to look further into this
         end
 
-        it "removes an apple from the tree" do
+        it 'removes an apple from the tree' do
             tree.add_apples
 
             apples_count = tree.apples.count
@@ -89,11 +89,11 @@ describe Tree do
     end
 
     describe '#dead?' do
-        it "returns false if the tree is alive" do
+        it 'returns false if the tree is alive' do
             expect(tree.dead?).to eq(false)
         end
 
-        it "returns true if the tree is dead" do
+        it 'returns true if the tree is dead' do
             tree.age_max.times do
                 tree.age!
             end
@@ -119,7 +119,7 @@ describe Fruit do
 end
 
 describe Apple do
-    let(:apple) { Apple.new("red", 5) }
+    let(:apple) { Apple.new('red', 5) }
 
     it 'should be a Class' do
         expect(described_class).to eq(Apple)
@@ -127,13 +127,13 @@ describe Apple do
 
     describe '#initialize' do
         it 'creates an apple of the specified color and diameter' do
-            expect(apple.color).to eq("red")
+            expect(apple.color).to eq('red')
             expect(apple.diameter).to eq(5)
         end
 
         it 'throws an argument error when not given the correct number of arguments' do
             expect{ Apple.new }.to raise_error(ArgumentError)
-            expect{ Apple.new("red") }.to raise_error(ArgumentError)
+            expect{ Apple.new('red') }.to raise_error(ArgumentError)
             expect{ Apple.new(5) }.to raise_error(ArgumentError)
         end
     end
