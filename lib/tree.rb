99bbaecd27de2e86a8ecaad448d1770b2d5bfc23
Justin Holm <jholm@covermymeds.com>
2017-05-12 10:16:12 -0400

    Justin Holm solution

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,38 +1,69 @@
 class NoApplesError < StandardError; end
 
-class AppleTree
-  attr_#fill_in :height, :age, :apples, :alive
+class Tree
+  attr_reader :height, :age, :apples, :alive
 
-  def initialize
+  def initialize(height = 0, age = 0, apples = [], alive = true)
+    @height = height
+    @age = age
+    @apples = apples
+    @alive = alive
   end
 
   def age!
+    # Age our tree by one year
+    @age += 1
+    
+    # Grow our tree a rand height between 1-3 feet per year
+    @height += rand(1..3)
+    
+    # Wait for our tree to age until 5, grow some fruit, then steal it
+    if @age >= 5
+      rand(100..300).times{ add_apples }
+    end
+  
+    # Our poor tree dies exactly on its 30th birthday, assuming its fruit has been picked
+    if @age >= 30
+      @alive = false
+    end
   end
 
   def add_apples
+    # Push a new red apple. Choose a random diameter size between 5-10 inch 
+    @apples.push Apple.new('red', rand(4..10))
   end
 
   def any_apples?
+    @apples.count > 0
   end
 
   def pick_an_apple!
     raise NoApplesError, "This tree has no apples" unless self.any_apples?
+    @apples.pop
   end
 
   def dead?
+    !@alive
   end
 end
 
 class Fruit
-  def initialize
-    has_seeds = true
-  end
-end
+  attr_reader :has_seeds
 
-class Apple <
-  attr_reader #what should go here 
+  def initialize
+    @has_seeds = true
+  end
+end 
+
+class Apple < Fruit
+  attr_reader :color, :diameter
 
   def initialize(color, diameter)
+    @color = color
+    @diameter = diameter
+
+    # Call our Fruit initialize method
+    super()
   end
 end
 
@@ -61,7 +92,7 @@ def tree_data
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    avg_diameter = basket.count > 0 ? diameter_sum / basket.count : 0
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
@@ -76,4 +107,4 @@ def tree_data
 end
 
 # Uncomment this line to run the script, but BE SURE to comment it before you try to run your tests!
-# tree_data
+#tree_data
