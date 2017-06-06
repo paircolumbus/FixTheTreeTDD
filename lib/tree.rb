8758d3cb122e8efcdd3881f1e4612f54ed020f17
John Chester <jchester@cmmc02th17xg8wl.innova.local>
2017-06-06 10:04:15 -0400

    Jack Chester Solutions

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,38 +1,60 @@
 class NoApplesError < StandardError; end
 
-class AppleTree
-  attr_#fill_in :height, :age, :apples, :alive
+class Tree
+  attr_accessor :height, :age, :apples, :alive
 
   def initialize
+    @height = 0
+    @age = 0
+    @apples = []
+    @alive = 0
   end
 
   def age!
+    @age += 1
+    @height += 1
+    @alive = false if @age > 15
+    add_apples if @age % 3 == 0
   end
 
   def add_apples
+    (1..10).each do |idx|
+      apple = Apple.new("Red", idx)
+      @apples.push(apple)
+    end
   end
 
   def any_apples?
+    return @apples.length > 0
   end
 
   def pick_an_apple!
     raise NoApplesError, "This tree has no apples" unless self.any_apples?
+    @apples.pop
   end
 
   def dead?
+    return !@alive
   end
 end
 
 class Fruit
+  attr_reader :has_seeds
+  
   def initialize
-    has_seeds = true
+    @has_seeds = true
   end
 end
 
-class Apple <
-  attr_reader #what should go here 
+class Apple < Fruit
+  attr_reader :diameter
+  attr_reader :color 
 
   def initialize(color, diameter)
+    @diameter = diameter
+    @color = color
+
+    super()
   end
 end
 
@@ -61,7 +83,7 @@ def tree_data
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    avg_diameter = if basket.size > 0; diameter_sum / basket.size else 0 end # It's up to you to calculate the average diameter for this harvest.
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
@@ -76,4 +98,4 @@ def tree_data
 end
 
 # Uncomment this line to run the script, but BE SURE to comment it before you try to run your tests!
-# tree_data
+#tree_data
