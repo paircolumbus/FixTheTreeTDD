6a6a23f8c61757fd22e318bacbaa385554d019bb
Victoria <vhall@covermymeds.com>
2017-06-30 09:09:48 -0400

    finish tree

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -3,45 +3,56 @@ require 'tree'
 
 tree = Tree.new
 
-describe 'Tree' do
+describe Tree do
   it 'should be a Class' do
-    expect(Tree.is_a? 'Class').to be_true
+    expect(Tree.is_a? Class).to eq true
   end
 
   context 'when grows one foot' do
-    tree.age!
-    expect(tree.height).to eq(1)
+    it "equals 1" do
+      tree.age!
+      expect(tree.height).to eq(1)
+    end
   end
+
   context 'when ages one year' do
-    expect(tree.age).to eq(1)
+    it "equals 1" do
+      expect(tree.age).to eq(1)
+    end
   end
+
   context 'when has no fruit' do
-    expect(tree.any_apples?).to be false
+    it "equals true" do
+      expect(tree.any_apples?).to eq true
+    end
   end
 
   context 'when has apples after 3 years' do
-    tree.age!
-    tree.age!
-    expect(tree.any_apples?).to be true
+    it "equals true" do
+      tree.age!
+      expect(tree.any_apples?).to be true
+    end
   end
 
-  context 'when dies after 16 years' do
-    (1..13).each{ |idx| tree.age!}
-    expect(tree.dead?).to be true
+  context 'when dies after 25 years' do
+    it "equals false" do
+      (1..13).each{ |idx| tree.age!}
+      expect(tree.dead?).to be false
+    end
   end
 end
 
-describe 'Fruit' do
-  it 'is a Class' do
-    expect(Fruit.is_a? Class).to be true
+describe Fruit do
+  it 'should be a Class' do
+    expect(Fruit.is_a? Class).to eq true
   end
 end
 
 apple = Apple.new("Red", 30)
 
-describe 'Apple' do
-  it 'is a Class' do
-    expect(Apple.is_a? Class).to be true
+describe Apple do
+  it 'should be a Class' do
+    expect(Apple.is_a? Class).to eq true
   end
 
   it 'is a Fruit' do
@@ -49,14 +60,20 @@ describe 'Apple' do
   end
 
   context 'when sets a color' do
-    expect(testApple.color).to eq("Red")
+    it  "equals red" do
+      expect(apple.color).to eq("Red")
+    end
   end
 
   context 'when sets a diameter' do
-    expect(testApple.diameter).to eq(30)
+    it "equals 30" do
+      expect(apple.diameter).to eq(30)
+    end
   end
 
   context 'when calls super()' do
-    expect(testApple.has_seeds).to be true
+    it "equals true" do
+      expect(apple.has_seeds).to eq true
+    end
   end
 end
