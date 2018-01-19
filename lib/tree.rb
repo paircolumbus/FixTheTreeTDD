f4080c0fb044b59dcc0c0553abccd96cb5b77f8e
Rupert Saxton <rsaxton@covermymeds.com>
2018-01-19 15:26:38 -0500

    Describe an apple tree that ages, grows, etc

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,38 +1,61 @@
 class NoApplesError < StandardError; end
 
+
 class AppleTree
-  attr_#fill_in :height, :age, :apples, :alive
+  attr_accessor :height, :age, :apples, :alive
 
   def initialize
+    @height = 0
+    @age = 0
+    @apples = []
+    @alive = true
+    @colors = ['red', 'green', 'yellow']
   end
 
   def age!
+    @age += 1
+    @height += rand(1..(2*@age))
+    self.add_apples
   end
 
   def add_apples
+    rand(1..(4*@age)).times do
+      @apples << Apple.new(@colors[rand(0..2)], rand(1..5))
+    end
   end
 
   def any_apples?
+    @apples.length > 0 ? true : false
   end
 
   def pick_an_apple!
     raise NoApplesError, "This tree has no apples" unless self.any_apples?
+    @apples.slice!(rand(0...@apples.length))
   end
 
   def dead?
+    if @height > 60 || @age > 13
+      true
+    else
+      false
+    end
   end
 end
 
 class Fruit
+  attr_reader :has_seeds
+
   def initialize
-    has_seeds = true
+    @has_seeds = true
   end
 end
 
-class Apple <
-  attr_reader #what should go here 
+class Apple < Fruit
+  attr_reader :color, :diameter 
 
   def initialize(color, diameter)
+    @color = color
+    @diameter = diameter
   end
 end
 
@@ -41,7 +64,7 @@ end
 # it should calculate the diameter of the apples in the basket
 
 def tree_data
-  tree = Tree.new
+  tree = AppleTree.new
 
   tree.age! until tree.any_apples?
 
@@ -61,7 +84,7 @@ def tree_data
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    avg_diameter = diameter_sum/basket.length
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
@@ -76,4 +99,4 @@ def tree_data
 end
 
 # Uncomment this line to run the script, but BE SURE to comment it before you try to run your tests!
-# tree_data
+tree_data
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,14 +1,54 @@
 require 'rspec'
 require 'tree'
 
-describe 'Tree' do
-  it 'should be a Class' do
-    expect(described_class.is_a? 'Class').to be_true
+describe AppleTree do
+  let(:tree) { AppleTree.new }
+
+  it { expect(tree.class).to be AppleTree  }
+
+  it 'will age' do
+  	tree.age!
+    expect(tree.age).to eq 1
+  end
+
+  it 'can add apples' do
+  	tree.age = 4
+    tree.add_apples
+    expect(tree.any_apples?).to be true
+  end
+
+  context 'when picking apples' do
+
+  	it 'raises an error for no apples' do
+  	  expect { tree.pick_an_apple! }.to raise_error(NoApplesError, "This tree has no apples")
+  	end
+
+  	it 'removes an apple when present' do
+  	  tree.age!
+  	  remaining_apples = tree.apples.length - 1
+  	  tree.pick_an_apple!
+  	  expect(tree.apples.length).to eq remaining_apples
+  	end
+
   end
 end
 
-describe 'Fruit' do
+describe Fruit do
+  let(:fruit) { Fruit.new }
+
+  it { expect(fruit.class).to be Fruit }
+
+  it { expect(fruit.has_seeds).to be true }
 end
 
-describe 'Apple' do
+describe Apple do
+  let(:apple) { Apple.new('red', 5) }
+
+  it "should have a color" do
+  	expect(apple.color).to eq 'red'
+  end
+
+  it 'should have a diameter' do
+  	expect(apple.diameter).to eq 5
+  end
 end
