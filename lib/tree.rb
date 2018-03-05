9c5624e0f7edb235e903bd503baf7d4e934ac5cd
Nick Caley <ncaley@covermymeds.com>
2018-03-05 09:46:29 -0500

    Completed Tree with tests

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,40 +1,76 @@
 class NoApplesError < StandardError; end
+class DeadTreeError < StandardError; end
 
 class Tree
-  attr_#fill_in :height, :age, :apples, :alive
 
-  def initialize
+  attr_reader :height
+  attr_reader :age
+  attr_reader :apples
+  attr_accessor :alive
+
+  def initialize(height = 0, age = 0, apples = [], alive = true)
+    @height = height
+    @age = age
+    @apples = apples
+    @alive = alive
   end
 
   def age!
+    raise DeadTreeError, "This tree is dead" if self.dead?
+    @age += 1
+
+    # Simple check if a tree has become too old
+    if @age >= 10
+      @alive = false
+    end
+
+    # What to do when a tree is alive and is aging
+    if @alive
+      @height += 1
+      add_apples
+    end
+
   end
 
   def add_apples
+    raise DeadTreeError, "This tree is dead" if self.dead?
+    # Change this if you want to change how apples are added/removed
+    count = @height
+    count.times { @apples.push(Apple.new('red', 1)) }
   end
 
   def any_apples?
+    @apples.size > 0
   end
 
   def pick_an_apple!
     raise NoApplesError, "This tree has no apples" unless self.any_apples?
+    @apples.shift
   end
 
   def dead?
+    @alive == false
   end
 end
 
 class Fruit
+  attr_reader :has_seeds
+
   def initialize
-    has_seeds = true
+    @has_seeds = true
   end
 end
 
-class Apple <
-  attr_reader #what should go here 
+class Apple < Fruit
+  attr_reader :color, :diameter
 
   def initialize(color, diameter)
+    super()
+    @color = color
+    @diameter = diameter
   end
 end
+
 
 #THERES ONLY ONE THING YOU NEED TO EDIT BELOW THIS LINE
 # avg_diameter (line 58) will raise an error.
@@ -61,7 +97,7 @@ def tree_data
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    avg_diameter = diameter_sum / basket.size.to_f # It's up to you to calculate the average diameter for this harvest.
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,14 +1,100 @@
 require 'rspec'
 require 'tree'
 
+def kill_tree(tree)
+  10.times { tree.age! }
+end
+
 describe Tree do
   it 'should be a Class' do
     expect(described_class.is_a? Class).to eq true
   end
+
+  describe '#pick_an_apple!' do
+    it 'should raise when there are no apples' do
+      tree = Tree.new
+      expect { tree.pick_an_apple! }.to raise_error(NoApplesError)
+    end
+
+    it 'shoud remove an apple' do
+      tree = Tree.new
+      tree.age!
+      tree.pick_an_apple!
+      expect(tree.any_apples?).to eq false
+    end
+  end
+
+  describe '#any_apples?' do
+    it 'should return true when there are apples' do
+      tree = Tree.new
+      tree.age!
+      expect(tree.any_apples?).to eq true
+    end
+
+    it 'should return false when there are not apples' do
+      tree = Tree.new
+      expect(tree.any_apples?).to eq false
+    end
+  end
+
+  describe '#age!' do
+    it 'should increase the age of the tree' do
+      tree = Tree.new
+      tree.age!
+      expect(tree.age).to eq 1
+    end
+
+    it 'should increase the height of the tree' do
+      tree = Tree.new
+      tree.age!
+      expect(tree.height).to eq 1
+    end
+
+    it 'should raise when the tree is dead' do
+      tree = Tree.new
+      kill_tree(tree)
+      expect { tree.age! }.to raise_error(DeadTreeError)
+    end
+  end
+
+  describe '#dead?' do
+    it 'should return true when the tree is not alive' do
+      tree = Tree.new
+      kill_tree(tree)
+      expect(tree.dead?).to eq true
+    end
+    
+    it 'should return false when the tree is alive' do
+      tree = Tree.new
+      expect(tree.dead?).to eq false
+    end
+  end
+
+  describe '#add_apples' do
+    it 'should raise when the tree is dead' do
+      tree = Tree.new
+      kill_tree(tree)
+      expect { tree.add_apples }.to raise_error(DeadTreeError)
+    end
+
+    it 'should add apples ' do
+      tree = Tree.new
+      expect(tree.any_apples?).to eq false
+      tree.age!
+      expect(tree.any_apples?).to eq true
+    end
+  end
+
 end
 
-describe 'Fruit' do
+describe Fruit do
+
 end
 
-describe 'Apple' do
+describe Apple do
+  it 'should have seeds when created' do
+    apple = Apple.new(1, 'blue')
+    has_seeds = apple.has_seeds
+    expect(has_seeds).to eq true
+  end
 end
