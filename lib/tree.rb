d43a40a9889c7711df8ad0fa22825bd2c065e7f4
Raghav Raman <rraman@covermymeds.com>
2018-05-14 13:51:55 -0400

    Completed

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,38 +1,61 @@
 class NoApplesError < StandardError; end
 
 class Tree
-  attr_#fill_in :height, :age, :apples, :alive
+  attr_reader :height, :age, :apples, :alive
 
-  def initialize
+  def initialize #constructor to initalize objects
+    @height = 1
+    @age = 1
+    @apples = []
+    @alive = true
   end
 
   def age!
+    @age += 1 
+    @height += 1
+    @alive = false if @age > 15 
+    add_apples if @age > 3
   end
 
   def add_apples
+    colors = ["red","green"]
+    diameter = rand(1..10)
+    number_of_apples = rand(10..20)
+    number_of_apples.times do 
+      apple = Apple.new(colors.sample,diameter)
+      @apples << apple
+    end
   end
 
   def any_apples?
+    apples.length > 0
   end
 
   def pick_an_apple!
     raise NoApplesError, "This tree has no apples" unless self.any_apples?
+    apple = apples.last
+    @apples.pop
+    apple
   end
 
   def dead?
+    !alive
   end
 end
+
 
 class Fruit
+  attr_reader :has_seeds
   def initialize
-    has_seeds = true
+    @has_seeds = true
   end
 end
 
-class Apple <
-  attr_reader #what should go here 
-
+class Apple < Fruit
+  attr_reader :has_seeds, :color, :diameter 
   def initialize(color, diameter)
+    @color = color
+    @diameter =  diameter
   end
 end
 
@@ -61,7 +84,7 @@ def tree_data
       diameter_sum += apple.diameter
     end
 
-    avg_diameter = # It's up to you to calculate the average diameter for this harvest.
+    avg_diameter = diameter_sum / basket.length # It's up to you to calculate the average diameter for this harvest.
 
     puts "Year #{tree.age} Report"
     puts "Tree height: #{tree.height} feet"
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -2,13 +2,94 @@ require 'rspec'
 require_relative '../lib/tree'
 
 describe Tree do
+  let(:treeOne) {described_class.new}
+
   it 'should be a Class' do
     expect(described_class.is_a? Class).to eq true
   end
+
+  context 'when tree is Alive' do
+  	it 'Should be alive with age =1, height = 1' do
+		expect(treeOne.age).to eq 1
+		expect(treeOne.height).to eq 1
+		expect(treeOne.dead?).to be false
+	end	
+
+	it 'Should Increment the age and height as aging' do
+		treeOne.age!
+		expect(treeOne.age).to eq 2
+		expect(treeOne.height).to eq 2
+		expect(treeOne.dead?).to be false
+	end
+
+	context 'when no apples' do
+		it 'Should have no apples unless added' do
+			expect(treeOne.any_apples?).to be false
+		end
+
+		it 'Should raise exception to pick an apple' do ##When expecting an error/exception pass it as a block of code
+			expect {
+				treeOne.pick_an_apple!
+			}.to raise_exception(NoApplesError)
+		end
+	end
+
+	context 'when apples added' do 
+		it 'Should add apples when add_apples is called' do
+			treeOne.add_apples
+			expect(treeOne.any_apples?).to be true
+		end
+
+		it 'Should decrement apples when picked' do
+			treeOne.add_apples
+			total_apples = treeOne.apples.length
+			treeOne.pick_an_apple!
+			expect(treeOne.apples.length).to eq (total_apples - 1)
+		end
+	end
+  end
+
+  context 'when tree is Dead' do
+  	it 'Should return true when the tree is dead and the dead method is called' do
+  		while treeOne.alive
+  			treeOne.age!
+  		end
+  		expect(treeOne.dead?).to be true
+	end  	
+
+	it 'Should have a possitive value for age and heigt' do
+		expect(treeOne.age).to be > 0
+		expect(treeOne.height).to be > 0
+  	end
+
+  end
 end
 
-describe 'Fruit' do
+describe Fruit do
+	let(:fruit) {described_class.new}
+
+	it 'should be a Class' do
+		expect(described_class.is_a? Class).to be true
+	end
+
+	it 'should have seeds' do
+		expect(fruit.has_seeds).to be true
+	end
 end
 
-describe 'Apple' do
+describe Apple do
+	let(:apple) {described_class.new("red",4)}
+
+	it 'should be a Class' do
+		expect(described_class.is_a? Class).to be true
+	end
+
+	it 'should be red in color' do
+		expect(apple.color).to eq "red"
+	end
+
+	it 'should be 4 in diameter' do
+		expect(apple.diameter).to eq 4
+	end
+
 end
