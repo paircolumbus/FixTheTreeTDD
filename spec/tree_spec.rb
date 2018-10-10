f0baefe92f95ad0fdb151dfd6e2d06b08c9093e2
matthew lee <matthewlee07@gmail.com>
2018-10-10 16:44:50 -0400

    .

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -5,10 +5,25 @@ describe Tree do
   it 'should be a Class' do
     expect(described_class.is_a? Class).to eq true
   end
+  it 'should create an instance of Tree with correct attributes' do
+    tree = Tree.new(5, 1, 0, true)
+    expect(tree.height).to eq(5);
+    expect(tree.age).to eq(1);
+    expect(tree.apples).to eq(0);
+    expect(tree.alive).to eq(true);
+  end
 end
 
-describe 'Fruit' do
+describe Fruit do
+  it 'should be a Class' do
+    expect(described_class.is_a? Class).to eq true
+  end
+  
 end
 
-describe 'Apple' do
+describe Apple do
+  it 'apple should be a an instance of Apple and subclass of Fruit' do
+    apple = Apple.new()
+    expect(apple).to be_a_kind_of(Fruit)
+  end
 end
